feabc4b5bf81e2438229519115cc20689936c1e5
Gabriel Morais <gabrielsmoraiss@gmail.com>
2018-04-28 13:00:58 -0300

    tutorial terminado

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import { AppRegistry } from 'react-native';
 import App from './src';
 
+//Fez funcionar a request que só dava erro com minha api node
+//Porque? não faço ideia.
+XMLHttpRequest = GLOBAL.originalXMLHttpRequest ? 
+  GLOBAL.originalXMLHttpRequest : GLOBAL.XMLHttpRequest;
+
 AppRegistry.registerComponent('ReactNativeApis', () => App);
--- src/index.js
+++ src/index.js
@@ -3,20 +3,85 @@ import {
   Button,
   StyleSheet,
   Text,
-  View
+  View,
+  AsyncStorage,
+  Alert
 } from 'react-native';
 
 import api from './services/api';
 
-export default class App extends Component<Props> {
+export default class App extends Component {
+  state = {
+    loggedInUser: null,
+    errorMessage: null,
+    projects: [],
+  };
+
   signIn = async () => {
-    const response = await api.post('/auth/authenticate')
+    
+    try {
+      const response = await api.post('/auth/authenticate', {
+        email: 'tesatee@teastee.com.br',
+        password: '12345678',
+      });
+
+      const { user, token } = response.data;
+
+      //console.log(user, JSON.stringify(user), token);
+      await AsyncStorage.multiSet([
+        ['@ReactNativeApis:token', token],
+        ['@ReactNativeApis:user', JSON.stringify(user)],
+      ]);
+      Alert.alert('Login com Sucesso!');
+
+      this.setState({ loggedInUser: user });
+
+    } catch (response) {
+      if (response.data) {
+        this.setState({ errorMessage: response.data.error });
+      } else {
+        console.log(response);
+      }
+    }
+  };
+
+  getProjectList = async () => {
+    try {
+      const response = await api.get('/projects');
+
+      const { projects } = response.data;
+
+      this.setState({ projects });
+    } catch (response) {
+      this.setState({ errorMessage: response.data.error });
+    }
+  };
+
+  async componentDidMount() {
+    const token = await AsyncStorage.getItem('@ReactNativeApis:token');
+    const user = JSON.parse(await AsyncStorage.getItem('@ReactNativeApis:user'));
+
+    if (token && user) {
+      this.setState({ loggedInUser: user });
+    }
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <Button onPress={this.signIn} title="Entrar" />
+        { !!this.state.loggedInUser && <Text> { this.state.loggedInUser.name } </Text> }
+        { !!this.state.errorMessage && <Text> { this.state.errorMessage } </Text> }
+        { this.state.loggedInUser ?
+          <Button onPress={this.getProjectList} title="Carregar" /> :
+          <Button onPress={this.signIn} title="Entrar" />
+        }
+        { this.state.projects.map(project => (
+          <View key={project._id} style={{ marginTop: 15 }}>
+            <Text style={{ fontWeight: 'bold' }}>{project.title}</Text>
+            <Text>{project.description}</Text>
+
+          </View>
+        ))}
       </View>
     );
   }
